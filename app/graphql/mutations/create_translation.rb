12bc1b7a9cfc34d38a7bb980b91bf42189c5cf0b
Matt Williams <matt.seongjun@gmail.com>
2019-08-09 11:37:10 -0400

    mutations work

--- app/graphql/mutations/create_translation.rb
+++ app/graphql/mutations/create_translation.rb
@@ -3,18 +3,18 @@ module Mutations
         # arguments passed to the `resolved` method
         argument :title, String, required: true
         argument :content, String, required: true
-        argument :creator_id, Int, required: true
+        argument :translator_id, Int, required: true
         argument :text_id, Int, required: true
 
         # return type from the mutation
         type Types::TranslationType
 
-        def resolve(title: nil, content: nil, creator_id: nil, text_id: nil)
-            Link.create!(
+        def resolve(title: nil, content: nil, translator_id: nil, text_id: nil)
+            Translation.create!(
                 title: title,
                 content: content,
-                creator_id: creator_id,
-                text_id: text_id
+                translator_id: translator_id,
+                text_id: text_id,
             )
         end
     end
