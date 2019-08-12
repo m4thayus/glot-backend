71c7c9a0c1d471011630d95903a3a3ffbaa875e5
Matt Williams <matt.seongjun@gmail.com>
2019-08-12 15:28:43 -0400

    Fix name

--- app/graphql/types/auth_provider_username_input.rb
+++ app/graphql/types/auth_provider_username_input.rb
@@ -1,7 +1,7 @@
 module Types
     class AuthProviderUsernameInput < BaseInputObject
         # the name is usually inferred by class name but can be overwritten
-        graphql_name 'AUTH_PROVIDER_EMAIL'
+        graphql_name 'AUTH_PROVIDER_USERNAME'
 
         argument :username, String, required: true
         argument :password, String, required: true
