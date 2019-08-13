75faa9d35cf16489249b47ba6ef47905560e427b
Matt Williams <matt.seongjun@gmail.com>
2019-08-13 12:16:10 -0400

    add context when creating new translations

--- app/graphql/mutations/create_translation.rb
+++ app/graphql/mutations/create_translation.rb
@@ -3,17 +3,16 @@ module Mutations
         # arguments passed to the `resolved` method
         argument :title, String, required: true
         argument :content, String, required: true
-        argument :translator_id, Int, required: true
         argument :text_id, Int, required: true
 
         # return type from the mutation
         type Types::TranslationType
 
-        def resolve(title: nil, content: nil, translator_id: nil, text_id: nil)
+        def resolve(title: nil, content: nil, text_id: nil)
             Translation.create!(
                 title: title,
                 content: content,
-                translator_id: translator_id,
+                translator_id: context[:current_user].id,
                 text_id: text_id,
             )
         end
