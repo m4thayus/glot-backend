4d6d77c4c9a3d57cbdc71e12cb434b5b21e3ba14
Matt Williams <matt.seongjun@gmail.com>
2019-08-20 09:42:29 -0400

    be less opinionated about status

--- app/graphql/mutations/create_translation.rb
+++ app/graphql/mutations/create_translation.rb
@@ -4,17 +4,18 @@ module Mutations
         argument :title, String, required: true
         argument :content, String, required: true
         argument :text_id, Int, required: true
+        argument :status, String, required: true
 
         # return type from the mutation
         type Types::TranslationType
 
-        def resolve(title: nil, content: nil, text_id: nil)
+        def resolve(title: nil, content: nil, text_id: nil, status: nil)
             Translation.create!(
                 title: title,
                 content: content,
                 translator_id: context[:current_user].id,
                 text_id: text_id,
-                status: 'pending'
+                status: status
             )
         end
     end
